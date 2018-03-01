4d309bdcdb0bfce4b02daa12fb33152cc5861aca
Cristopher Garduno Luna <garduno.cris@gmail.com>
2018-03-01 07:46:35 -0800

    create autoUpload.sh

---
+++ autoUpload.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# File    : autoUpload.sh
+# Purpose : Upload example data to github 
+# Author  : Cristopher Garduno Luna
+# Date Created:      : 03-01-2018
+# Date Last Modified : 03-01-2018
+
+# update images folder on git
+git add images
+git commit -m "updated images"
+git push
